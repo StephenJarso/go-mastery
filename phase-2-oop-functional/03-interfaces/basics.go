8e9b3d7e7b821ed1de6e8eb9ee3b6859e274195f
StephenJarso <stephenjacob815@gmail.com>
2026-06-21 22:33:32 +0300

    Phase 2.3.1: Implemented a function to show usage of interfaces interchangeably

--- phase-2-oop-functional/03-interfaces/basics.go
+++ phase-2-oop-functional/03-interfaces/basics.go
@@ -1,21 +1,42 @@
 package phase2oopfunctional
 
 import "fmt"
-//interfacebasics demonstrates declaring and implementing interfaces
-type Speaker interface{
+
+// interfacebasics demonstrates declaring and implementing interfaces
+// In GO,interfaces are satisfied IMPLICITLY -  there's no "implements" keyword
+// If a type has a right method it automatically satisfies the interface
+// Speaker is a simple interface with one method
+// Any type with speak() string satisfies this interface
+type Speaker interface {
 	speak() string
 }
-type Dog struct{
+
+// Dog satisfies Speaker by having a speak method
+type Dog struct {
 	Name string
 }
 
-type Cat struct{
+func (d Dog) speak() string {
+	return d.Name + " says woof!"
+}
+
+// Cat also satisfies Speaker - completely independent of Dog
+type Cat struct {
 	Name string
 }
 
-func(d Dog) speak()string{
-	return d.Name+" says woof!"
+func (c Cat) speak() string {
+	return c.Name + " says meow!"
 }
-func(c Cat) speak() string{
-	return c.Name+" says meow!"
+
+//basicInterfaceUsage shows that any Speaker can be used interchangeably
+func BasicInterfaceUsage() {
+	var s Speaker
+
+	s = Dog{
+		Name: "Rex",
+	}
+	fmt.Println(s.speak())
+	s = Cat{Name: "Whiskers"}
+	fmt.Println(s.speak())
 }
